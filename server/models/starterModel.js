aa18ae94d019d647d803aaacffb3e00809c6cff8
Rob Nobile <robert.nobile@gmail.com>
2020-09-28 16:35:23 -0400

    Initial commit

---
+++ server/models/starterModel.js
@@ -0,0 +1,20 @@
+
+// FOR SINGLE QUERY
+
+const { Pool } = require('pg');
+
+const PG_URI =
+  // Insert your URI
+  '<INSERT PG URI HERE>';
+
+const pool = new Pool({
+  connectionString: PG_URI,
+  connectionLimit: 300,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
