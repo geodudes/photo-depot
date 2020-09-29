88c4c088df37ea63eb9fca9052a1a0787bff135c
Rob Nobile <robert.nobile@gmail.com>
2020-09-29 00:16:47 -0400

    Setup extension - contextMenu

---
+++ chrome-extension/eventPage.js
@@ -0,0 +1,8 @@
+//==========="Background" Scripts===========//
+const contextMenuItem = {
+  id: "photoDepot",
+  title: "Photo Depot",
+  contexts: ["image"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
