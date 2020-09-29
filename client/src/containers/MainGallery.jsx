af235ce7771209306ad0ed128e6bf2d9ca676076
Rob Nobile <robert.nobile@gmail.com>
2020-09-29 19:13:33 -0400

    Set up main-gallery mapping

--- client/src/containers/MainGallery.jsx
+++ client/src/containers/MainGallery.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from './../actions/actions';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+  
+});
 
 const MainGallery = () => {
   return (
@@ -6,4 +16,4 @@ const MainGallery = () => {
   )
 }
 
-export default MainGallery;
+export default connect(mapStateToProps, mapDispatchToProps)(MainGallery);
