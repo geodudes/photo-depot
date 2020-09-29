3210c0642c18ed9c9a545f9566e55d59e7dc10c7
Rob Nobile <robert.nobile@gmail.com>
2020-09-29 02:49:52 -0400

    Set messages from background-script to popup-script

--- chrome-extension/eventPage.js
+++ chrome-extension/eventPage.js
@@ -1,8 +1,28 @@
 //==========="Background" Scripts===========//
-const contextMenuItem = {
+let contextMenuItem = {
   id: "photoDepot",
   title: "Photo Depot",
   contexts: ["image"]
 };
 
 chrome.contextMenus.create(contextMenuItem);
+
+// chrome.contextMenus.removeAll(function() {
+//   chrome.contextMenus.create(contextMenuItem);
+// });
+
+chrome.contextMenus.onClicked.addListener(image => {
+  // alert(`${image.srcUrl}`)
+  // image.srcUrl
+  // document.querySelector('#image-address').innerText = image.srcUrl;
+  // sessionStorage.setItem('TestURL', image.srcUrl)
+  // console.log('Saved')
+  chrome.runtime.sendMessage({
+    msg: "imageUrl_sent", 
+    data: {
+        subject: "Image url:",
+        content: image.srcUrl
+    }
+  });
+})
+
