5e93ce18ba54ad97f8ce8845c3936376157439f5
Rob Nobile <robert.nobile@gmail.com>
2020-09-29 13:52:30 -0400

    Send POST request and extract image-id from response

--- chrome-extension/eventPage.js
+++ chrome-extension/eventPage.js
@@ -10,27 +10,29 @@ chrome.contextMenus.create(contextMenuItem);
 
 // Event listener for when contect menu item is selected
 chrome.contextMenus.onClicked.addListener(image => {
+  const imageUrl = image.srcUrl;
   // Send request to server with image url - i.e. image.src.Url
-  fetch('/photos', {
+  fetch('http://localhost:3000/images', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      url: image.srcUrl
+      url: imageUrl
     })
   })
   .then(res => res.json())
-  .then(res => console.log('RESPONSE: ', res))
-  .catch(err => console.log('Error: ', err))
+  .then(res => {
 
-  // Send image url to popup as a message
-  chrome.runtime.sendMessage({
-    msg: "imageUrl_sent", 
-    data: {
-        subject: "Image url:",
-        content: image.srcUrl
-    }
-  });
+    res.photoid
+    // Send image url and response id, respectively, to popup
+    chrome.runtime.sendMessage({
+      msg: "imageUrl_sent", 
+      data: {
+          urlId: [imageUrl, res.photoid],
+      }
+    });
+  })
+  .catch(err => console.log('Error: ', err))
 })
 
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,10 +2,13 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
       if (request.msg === "imageUrl_sent") {
-          console.log(request.data.subject)
-          console.log(request.data.content)
-          const shortenedUrl = request.data.content.substring(0, 20).concat('...')
+          // Extract url and display
+          const shortenedUrl = request.data.urlId[0].substring(0, 20).concat('...')
           document.querySelector('#image-address').innerText = shortenedUrl;
+
+          // Extract image id and display
+          const imageId = request.data.urlId[1];
+          document.querySelector('#image-id').innerText = imageId;
       }
   }
 );
