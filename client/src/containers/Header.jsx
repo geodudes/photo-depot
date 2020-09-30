ddc5fab362b34ba65d5a531d49e9c827701965b1
Rob Nobile <robert.nobile@gmail.com>
2020-09-30 00:50:13 -0400

    Update chrome extension icons

--- client/src/containers/Header.jsx
+++ client/src/containers/Header.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Button, Navbar } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <div id="header">
-      <Button variant="outline-primary">Primary</Button>{' '}
+      <Button variant="outline-primary">Primary</Button>
     </div>
-    // <Navbar bg="light" variant="light">
-    //   <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    //   <Nav className="mr-auto">
-    //     <Nav.Link href="#home">Home</Nav.Link>
-    //     <Nav.Link href="#features">Features</Nav.Link>
-    //     <Nav.Link href="#pricing">Pricing</Nav.Link>
-    //   </Nav>
-    //   <Form inline>
-    //     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-    //     <Button variant="outline-primary">Search</Button>
-    //   </Form>
-    // </Navbar>
+
   )
 }
 
